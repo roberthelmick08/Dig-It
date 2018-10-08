f52cc37068698fc87d17cc994d43e243c8a43aaa
Robert Helmick <roberthelmick@RobertMacbookPro.frontierlocal.net>
2018-10-07 23:53:07 -0500

    add plant dialog

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,21 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MatIconModule, MatDialog, MatDialogModule, MatButtonModule } from '@angular/material';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { AddPlantDialogComponent } from './search/add-plant-dialog/add-plant-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    AddPlantDialogComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
+    OverlayModule,
+    MatDialogModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  entryComponents: [
+    AddPlantDialogComponent,
+  ],
+  providers: [MatDialog],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/search/search.component.ts
+++ client/src/app/search/search.component.ts
@@ -1,25 +1,38 @@
+import { AddPlantDialogComponent } from './add-plant-dialog/add-plant-dialog.component';
 import { DataService } from './../data.service';
 import { Plant } from './../../models/plant';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
-  providers: [DataService]
+  providers: [ DataService ]
 })
 export class SearchComponent implements OnInit {
 
   plantsList: Array<Plant> = [];
 
-  constructor( private dataService: DataService) { }
+  constructor( private dataService: DataService, public dialog: MatDialog ) { }
 
-  ngOnInit() { this.getPlants(); }
+  ngOnInit() {
+    this.getPlants();
+  }
 
   getPlants() {
     this.dataService.getAllPlants()
         .subscribe(plants => this.plantsList = plants);
-    console.log('Data from dataService', this.plantsList);
   }
 
+  openAddPlantDialog() {
+    const dialogRef = this.dialog.open(AddPlantDialogComponent, {
+      height: '60vh',
+      width: '40%'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // TODO
+    });
+  }
 }
