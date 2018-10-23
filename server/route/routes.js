4820e2150d68c8abbf67d1a71d2f0783f331cfb2
Robert Helmick <roberthelmick@RobertMacbookPro.frontierlocal.net>
2018-10-23 00:12:05 -0500

    addPlantToGarden  POST success

--- server/route/routes.js
+++ server/route/routes.js
@@ -91,22 +91,24 @@ router.put('/garden', auth, (req, res) => {
             "message": "UnauthorizedError: private profile"
         });
     } else {
-        User.findOneAndUpdate({ _id: req.params.id }, {
+        User.findOneAndUpdate({ _id: req.body._id }, {
                 $set: {
-                    _id: req.params._id,
-                    name: req.params.name,
-                    email: req.params.email,
-                    admin: req.params.admin,
-                    phone: req.params.phone,
-                    zone: req.params.zone,
-                    zip: req.params.zip,
-                    garden: req.params.garden,
+                    name: req.body.name,
+                    email: req.body.email,
+                    admin: req.body.admin,
+                    phone: req.body.phone,
+                    zone: req.body.zone,
+                    zip: req.body.zip,
+                    garden: req.body.garden,
                 }
             },
+            // flag to return newly updated mongo document
+            { new: true },
             function(err, result) {
                 if (err) {
-                    console.log(err);
+                    res.json(err);
                 } else {
+                    res.json(result);
                     console.log(result);
                 }
             });
@@ -163,7 +165,7 @@ router.post('/new_plant', (req, res, next) => {
 
 // PUT edit plant
 router.put('/plant/:id', (req, res, next) => {
-    PlantSchema.findOneAndUpdate({ _id: req.params.id }, {
+    PlantSchema.findOneAndUpdate({ _id: req.body._id }, {
             $set: {
                 botanicalName: req.body.botanicalName,
                 commonName: req.body.commonName,
@@ -198,7 +200,7 @@ router.put('/plant/:id', (req, res, next) => {
 
 // DELETE plant from DB
 router.delete('/plant/:id', (req, res, next) => {
-    PlantSchema.remove({ _id: req.params.id }, function(err, result) {
+    PlantSchema.remove({ _id: req.body._id }, function(err, result) {
         if (err) {
             res.json(err);
         } else {
