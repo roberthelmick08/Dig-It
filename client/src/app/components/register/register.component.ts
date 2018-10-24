7fcc5343370cc2b8c8cf065553b1c813d3b802c9
Robert Helmick <roberthelmick@RobertMacbookPro.frontierlocal.net>
2018-10-24 00:54:18 -0500

    CORS error bugfixing

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { AuthenticationService, TokenPayload } from '../../services/authentication.service';
 import { Router } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-register',
@@ -16,18 +17,26 @@ export class RegisterComponent {
     password: '',
     admin: false,
     phone: null,
-    zone: null,
+    zone: 8,
     zip: null,
     garden: []
-
   };
 
   @Output()
   navigateToGardenEvent = new EventEmitter();
 
-  constructor(private auth: AuthenticationService, private router: Router) {}
+  constructor(private auth: AuthenticationService, private dataService: DataService, private router: Router) {}
 
   register() {
+
+    // TODO: API calls to determine zone, first frost date and last frost date from zip
+    // this.dataService.getZoneFromZip(this.credentials.zip).subscribe(zone => {
+    //   this.credentials.zone = zone;
+    //   console.log(this.credentials.zone);
+    // }, (err) => {
+    //   console.error(err);
+    // });
+
     this.auth.register(this.credentials).subscribe(() => {
       this.navigateToGardenEvent.emit(null);
     }, (err) => {
