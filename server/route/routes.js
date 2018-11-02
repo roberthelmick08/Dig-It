6c56be9cd23349a3b64f34279aff999f4f7fba24
Robert Helmick <roberthelmick@RobertMacbookPro.frontierlocal.net>
2018-11-01 20:34:20 -0500

    log cleanup

--- server/route/routes.js
+++ server/route/routes.js
@@ -69,7 +69,6 @@ router.post('/login', (req, res, next) => {
 });
 
 router.get('/user', auth, (req, res) => {
-    console.log('in user routes', req.payload._id);
     if (!req.payload._id) {
         res.status(401).json({
             "message": "UnauthorizedError: private profile"
